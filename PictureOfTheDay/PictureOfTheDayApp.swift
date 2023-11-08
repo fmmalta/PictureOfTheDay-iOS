71f594df0cb96ab51d88dbd6ab1a1026fd638ffb
fmmalta <fellipedmmalta@gmail.com>
2023-11-08 08:11:12 -0300

    Initial Commit

---
+++ PictureOfTheDay/PictureOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureOfTheDayApp.swift
+//  PictureOfTheDay
+//
+//  Created by Fellipe Malta on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PictureOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
